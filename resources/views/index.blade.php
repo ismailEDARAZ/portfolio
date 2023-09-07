975ad3aeb27aa58f5a01ce975c5f8350e710f98f
unknown <ismail.edaraz@gmail.com>
2023-09-07 21:55:53 +0100

    Change index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,9 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="{{ asset('assets/css/base.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/css/vendor.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css')}}">
